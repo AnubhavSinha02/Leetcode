edcea69c5c9c9b129c7197e9942d60031da05e48
Anubhav Sinha <34247291+AnubhavSinha02@users.noreply.github.com>
2026-09-04 00:17:25 +0530

    Time: 8 ms (50%), Space: 120.6 MB (89.13%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min = Arrays.stream(nums1).min().getAsInt();
+
+        if (min % 2 == 1) {
+            return true;
+        }
+
+        for (int num : nums1) {
+            if (num % 2 == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
